ef8546863cfd14fdda6b7765c18ed271b718093f
srisri96 <srikanthsitharth80@gmil.com>
2022-07-21 18:17:54 +0530

    Switch statement

---
+++ Emp_switch/Emp_switch/switch.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Emp_switch
+{
+    internal class Eswitch
+    {
+        public static void Emps()
+        {
+            const int fullTime = 1;
+            const int partTime = 2;
+            int empHr = 0;
+            int empWage = 0;
+            int wagePerHr = 20;
+            
+            Random random = new Random();
+            int attendCheck = random.Next(0, 3);
+            switch (attendCheck)
+            {
+                case fullTime:
+                    empHr = 8;
+                    Console.WriteLine("Full time employee");
+                    break;
+                case partTime:
+                    empHr = 4;
+                    Console.WriteLine("Part time employee");
+                    break;
+                default:
+                    empHr = 0;
+                    Console.WriteLine("The Employee is absent");
+                    break;
+            }
+            empWage = empHr * wagePerHr;
+            Console.WriteLine($"The Employee wage is {empWage}");
+        }
+    }
+}
