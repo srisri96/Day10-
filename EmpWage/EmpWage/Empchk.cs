b7feca7087f31dfe7ac54b39f24421e5c6e7d7eb
srisri96 <srikanthsitharth80@gmil.com>
2022-07-21 18:23:35 +0530

    employee

---
+++ EmpWage/EmpWage/Empchk.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpWage
+{
+    internal class Empchk
+    {
+        public static void Emppresent()
+        {
+            int fullTime = 1;
+            Random rnd = new Random();
+            int attendCheck = rnd.Next(0, 2);
+            if (attendCheck == fullTime)
+            {
+                Console.WriteLine("The Employee is present");
+            }
+            else
+            {
+                Console.WriteLine("The Employee is absent");
+            }
+        }
+    }
+}
---
+++ EmpWage/EmpWage/Program.cs
@@ -0,0 +1,2 @@
+﻿using EmpWage;
+Empchk.Emppresent();
