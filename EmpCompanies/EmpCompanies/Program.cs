029c530de5dd6a4ea37584e2498f29c5b27ef58d
srisri96 <srikanthsitharth80@gmil.com>
2022-07-21 18:21:06 +0530

    Companies

---
+++ EmpCompanies/EmpCompanies/Program.cs
@@ -0,0 +1,5 @@
+﻿using EmpCompanies;
+Console.WriteLine("Welcome to employee wage computation program");
+empwage emp = new empwage();
+emp.Wage("TCS", 20);
+emp.Wage("Zoho", 30);
---
+++ EmpCompanies/EmpCompanies/empwage.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpCompanies
+{
+    internal class empwage
+    {
+      
+            public int fullTime = 1;
+        int empHr = 0;
+        int empWage = 0;
+        public void Wage(string comp, int wagePerHr)
+        {
+            Random random = new Random();
+            int attendCheck = random.Next(0, 2);
+            if (attendCheck == fullTime)
+            {
+                empHr = 8;
+                Console.WriteLine($"The Employee is present");
+            }
+            else
+            {
+                empHr = 0;
+                Console.WriteLine($"The Employee wage is absent");
+            }
+            empWage = empHr * wagePerHr;
+            Console.WriteLine($"The Empployee wage is {empWage}");
+        }
+    }
+}
