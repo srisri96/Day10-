8e9914de2829561182ba3afa3501d578f1359c11
srisri96 <srikanthsitharth80@gmil.com>
2022-07-21 18:22:51 +0530

    employeewage 9

---
+++ Employeewage9/Employeewage9/Empwage.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employeewage9
+{
+    internal class Empwage
+    {
+        public int fullTime = 1;
+        public int partTime = 2;
+        public int empHr = 0;
+        public int empWage = 0;
+        public int totalWage = 0;
+        public void Wage(string comp, int wagePerHR, int totalWorkingdays, int totalWorkingHR)
+        {
+            for (int i = 0; i < totalWorkingdays; i++)
+            {
+                
+                Random random = new Random();
+                int attendCheck = random.Next(0, 3);
+                if (attendCheck == fullTime)
+                {
+                    empHr = 8;
+                }
+                else if (attendCheck == partTime)
+                {
+                    empHr = 4;
+                }
+                else
+                {
+                    empHr = 0;
+                }
+                empWage = empHr * wagePerHR;
+                totalWage = totalWage + empWage;
+            }
+        }
+    }
+}
---
+++ Employeewage9/Employeewage9/Program.cs
@@ -0,0 +1,8 @@
+﻿using Employeewage9;
+Console.WriteLine("Welcome to employee wage computation program");
+Empwage emp1 = new Empwage();
+emp1.Wage("TCS", 20, 20, 100);
+Empwage emp2 = new Empwage();
+emp2.Wage("Zoho", 30, 20, 100);
+Console.WriteLine($"Total wage for an employee of TCS is {emp1.totalWage}");
+Console.WriteLine($"Total wage for an employee of Zoho is {emp2.totalWage}");
